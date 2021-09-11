4d908b1c7a7841336c95a6399587193df32cc592
Gautameshwar <42206163+Gautameshwar@users.noreply.github.com>
2021-09-11 16:20:03 +0530

    Made the algo run faster. Introduced class and alternate energy fn to speed up calculations!

--- data_gen.py
+++ data_gen.py
@@ -1,63 +1,99 @@
-###create a random 40x40 array of up-down spin (for 2d ising lattice. 1d array for 1d lattice)
+#optimised monte carlo by introducing classes and energy functions...
 import numpy as np
 import matplotlib.pyplot as plt
 
-#dimension of our array
-d=40
+#define an ising model class to simplify calculations...
+class ising_model_2D():
+    #inputs our lattice, keeps the data of our lattice's energy and spin tagged
+    def __init__(self,d,a,mfield):
+        #lattice with a:(1-a) down-up
+        temp=np.random.random((d,d))
+        
+        temp[temp>=a]=1
+        temp[temp<a]=-1
+        
+        #create 0 in the border (just to make neighbour atom calculations easier)
+        temp=np.pad(array=temp, pad_width=1, mode='constant')
+        self.lattice=temp.copy()
+        del temp
+        
+        self.b=mfield
 
-lattice=np.random.random((d,d))
-#lattice with a:(1-a) down-up
-a=0.8
-
-lattice[lattice>=a]=1
-lattice[lattice<a]=-1
-
-#create 0 in the border (just to make neighbour atom calculations easier)
-lattice=np.pad(array=lattice, pad_width=1, mode='constant')
-
-###define a function to calculate the total energy of our lattice
-def get_energy(lattice):
-    e=0
-    #look at the i,jth atom. 
-    for i in range(1,d):
-        for j in range(1,d):
-            #see the neighbouring atoms(up,down,left,right)
-            g=np.array([[0,1,0],[1,0,1],[0,1,0]])
-            
-            #calculate the sigma_ij*sigma_i'j' around our atom at ij (stored in matrix form)
-            m=-lattice[i,j]*g*lattice[i-1:i+2,j-1:j+2]
-            
-            #sum over all the neighbour sigmas
-            e+=np.sum(m)
-            # print(e,m,np.sum(m),i,j)
+        #initialise the net spin of our lattice
+        self.spin=np.sum(self.lattice)
+        
+    #fn to find the energy of our lattice with B
+    def get_energy(self):
+        e=0
+        #look at the i,jth atom. 
+        for i in range(1,d):
+            for j in range(1,d):
+                #see the neighbouring atoms(up,down,left,right)
+                g=np.array([[0,1,0],[1,0,1],[0,1,0]])
+                
+                #calculate the sigma_ij*sigma_i'j' around our atom at ij
+                m=-self.lattice[i,j]*g*self.lattice[i-1:i+2,j-1:j+2]
+                
+                #sum over all the neighbour sigmas
+                e+=np.sum(m)-self.b*self.lattice[i,j]
+        
+        return e
     
-    return e
+    #fn to calculate the energy of lattice on flipping spin at (x,y)
+    def flipcheck(self,x,y):
+        #the energy change comes only from the neighbours of the spin we flipped at (x,y)
+        g=np.array([[0,1,0],[1,0,1],[0,1,0]])
+        
+        #calculate the sigma_ij*sigma_i'j' around our atom at ij
+        m=-self.lattice[x,y]*g*self.lattice[x-1:x+2,y-1:y+2]
+        
+        #sum over all the neighbour sigmas and the effect of B
+        e1=np.sum(m)-self.lattice[x,y]*self.b
+        
+        #flip the (x,y)th spin
+        self.lattice[x,y]*=-1
+        
+        # print('in flip fn', self.lattice)
+        
+        #calculate the sigma_ij*sigma_i'j' around our atom at ij
+        m=-self.lattice[x,y]*g*self.lattice[x-1:x+2,y-1:y+2]
+        
+        #sum over all the neighbour sigmas and the effect of B
+        e2=np.sum(m)-self.lattice[x,y]*self.b
+        
+        #flip bacc the (x,y)th spin
+        self.lattice[x,y]*=-1
+        # print('after flip fn', self.lattice)
+        return e1,e2
 
-  
-#just to check if our energy calculation makes sense for special cases:
-#the most unstable energy(unalligned spins)
-m=np.ones((d,d))
-m[1::2,1::2]=-1
-m[::2,::2]=-1
-m=np.pad(array=m, pad_width=1, mode='constant')
+    #fn to flip the (x,y)th spin
+    def flip(self,x,y):
+        #spin at (x,y) flippd
+        self.lattice[x,y]*=-1
+        
+        #manually update the energy of our system after the iterations...
+        
+        #update spin
+        self.spin+=2*self.lattice[x,y]
+    
 
-#the most stable energy(alligned spins))
-n=np.ones((d,d))
-n=np.pad(array=n, pad_width=1, mode='constant')
+#dimension of our array
+d=200
 
+#optional addition of external magnetic field B
+b=0.0
+lat=ising_model_2D(d,0.5,b)
 
-print(get_energy(lattice),get_energy(m),get_energy(n))
-
-#the ising model generated(white->up, black->down)
+print(lat.get_energy(),lat.spin,'starting!')
+lattice=lat.lattice.copy()
 plt.imshow(lattice,cmap='Greys_r',interpolation='nearest', origin='lower')
 
-###Implement metropolis algo (Source Wikipedia) // How reliable is this algo? Can we provide a guarantee of our datas quality?
 #metropolis algo
-def metro(lattice,reps):
+def metro(lat,reps):
     #our inverse temperature
-    beta=.5
+    beta=.75
     
-    #for plotting our monte carlo data
+    #to record our monte carlo data
     iterate,energy,spin=np.zeros(reps),np.zeros(reps),np.zeros(reps)
     
     for i in range(reps):
@@ -65,13 +101,8 @@ def metro(lattice,reps):
         #random pick of our point in lattice
         x,y=np.random.randint(1,d),np.random.randint(1,d)
         
-        #make a copy of lattice with the spin at our random point flipped
-        lattice_flip=lattice.copy()
-        lattice_flip[x,y]*=-1
-        
         #energies of our two systems
-        e1=get_energy(lattice)
-        e2=get_energy(lattice_flip)
+        e1,e2=lat.flipcheck(x,y)
         
         #if energy increase only keep with a probability(more energy increase, lesser the prob)
         if e2>e1:
@@ -80,31 +111,37 @@ def metro(lattice,reps):
             
             if np.random.random()<p:
                 
-                lattice[x,y]*=-1
+                lat.flip(x,y)
             
         #flip the lattice spin if energy decreases
         else:
-            lattice[x,y]*=-1
+            lat.flip(x,y)
         
-        #dont need the copy after comparison
-        del lattice_flip
-        
-        #save the energy
-        energy[i]=get_energy(lattice)
+        #save the energy in case yu need to see the convergence of monte carlo..
+        #energy[i]=lat.energy
         iterate[i]=i
-        spin[i]=np.sum(lattice)
+        spin[i]=lat.spin
+        
+    
     return iterate,energy,spin
 
-#we have the lattice configuration generated for our specified temperature β.
-x,y,z=metro(lattice,10)
+
+x,drop,y=metro(lat,1000000)
+
+#if you wanna plot the energy plots for every monte iteration, use this
 plt.figure()
 plt.grid()
 plt.plot(x,y)
-plt.plot(x,z)
-plt.show()    
+# plt.plot(x,z)
+plt.show()
 
-plt.imshow(lattice,cmap='Greys_r',interpolation='nearest', origin='lower')
+print(lat.get_energy(),lat.spin)
+plt.imshow(lat.lattice,cmap='Greys_r',interpolation='nearest', origin='lower')
 
-#generate as much data as you require for all sets of temperatures.
+#this is one such generation of our ising model once it attained sufficient iterations.
+#generate as much data as you require for all sets of temperatures.(by varying beta in metro algo...)
 
-#try out animating the plots as monte carlo runs..?
+#try out animating the plots as monte carlo runs:
+#at the end of each iteration of metro algo, we have a new state of our spin system as a numpy matrix of 1 and -1 (0 in the borders)
+#convert this into a image, append it to an array. at the end of metro algo, we have the snapshots of all our monte carlo iterations.!
+#convert this set of image arrays into a gif with appropriate fps
